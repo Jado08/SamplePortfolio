8e222d8e77d8179bbf724e59f58d9f9fc745e032
Jado08 <julieanndingalorcine@gmail.com>
2024-08-28 15:05:42 +0800

    just try

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline bg-yellow-500">
-      Hello world!
+      I Love You Virus!
     </h1>
   )
 }
